7cfcaa8ecc3568719573698dd5cbc8a807dd290d
AlexRamey <alex.b.ramey@gmail.com>
2014-08-02 23:30:46 -0400

    Background App Refresh Implemented New Assets Dropped In Started Event Detail View

---
+++ Charlottesville Artfinder/ARTEventDetailView.h
@@ -0,0 +1,13 @@
+//
+//  ARTEventDetailView.h
+//  Charlottesville Artfinder
+//
+//  Created by Alex Ramey on 8/2/14.
+//  Copyright (c) 2014 HooApps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ARTEventDetailView : UIView
+
+@end
---
+++ Charlottesville Artfinder/ARTEventDetailView.m
@@ -0,0 +1,31 @@
+//
+//  ARTEventDetailView.m
+//  Charlottesville Artfinder
+//
+//  Created by Alex Ramey on 8/2/14.
+//  Copyright (c) 2014 HooApps. All rights reserved.
+//
+
+#import "ARTEventDetailView.h"
+
+@implementation ARTEventDetailView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
